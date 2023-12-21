bb4a2f32775bd3fd640bcbb389cc0f4e5b598be9
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2023-12-21 21:25:37 +0100

    dsds

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth");
 
-router.post("/login", (req, res) => {});
-
+router.post("/login", authController.login);
+router.post("/register", authController.register);
 module.exports = router;
